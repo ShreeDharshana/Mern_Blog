78359cdf6e11884848ecad92e31b8c7c3b0cdebb
Shree Dharshana <shreedharshana25@gmail.com>
2024-08-26 14:13:32 +0530

    create a test api and route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 const app = express();
+
 mongoose
   .connect(process.env.mongo)
   .then(() => {
@@ -16,3 +18,5 @@ mongoose
 app.listen(3000, () => {
   console.log("server running on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,4 @@
-import { timeStamp } from "console";
 import mongoose from "mongoose";
-import { type } from "os";
 
 const userSchema = new mongoose.Schema(
   {
